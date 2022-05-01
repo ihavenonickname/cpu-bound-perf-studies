fb6718e89de48290c2f5520dc10565daae707d01
Gabriel <gbsm1993@gmail.com>
2022-05-01 06:05:35 -0300

    better startup message

--- graph-node/server.js
+++ graph-node/server.js
@@ -25,7 +25,7 @@ app.post('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`listening at http://localhost:${port}`)
 })
 
 function convertFromMap(components) {
